fc01d6ee5339e2ce3f58cdd8113aa3753e4e0747
johann <johann.cavallucci@epitech.eu>
2025-05-30 21:26:18 +0200

    add initial setup for Nuxt 3 project with Tailwind CSS integration

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  modules: ['@nuxt/ui'],
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4
+  },
+  css: ['~/assets/index.css'],
+
+})
