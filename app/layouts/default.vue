5378c78eb85579ce3d2a82a6b2c78e4b35dca6ee
johann <johann.cavallucci@epitech.eu>
2025-05-31 13:24:05 +0200

    refactor canvas drawing component and restructure app layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,12 @@
 <template>
-    <slot />
+  <div class="scrollbar-hide flex min-h-screen w-full flex-col overflow-hidden">
+    <LayoutNavBar />
+    <div class="px-4">
+      <slot />
+    </div>
+    <LayoutFooter />
+  </div>
 </template>
+
+<script setup lang="ts">
+</script>
